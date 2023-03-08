0edcd20ff003b30d5b12a70a3f82c910fd1d3529
BingYou Leong <byleong1990@gmail.com>
2023-03-08 14:48:46 +0800

    added seded data to restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+5.times do
+  Restaurant.create({ name: 'akak', address: 'ititit', phone_number: 727_272, category: 'japanese' })
+end
