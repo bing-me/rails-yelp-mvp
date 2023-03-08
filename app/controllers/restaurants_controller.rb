7387ad300086a607239b115df671095fede48021
BingYou Leong <byleong1990@gmail.com>
2023-03-08 15:45:06 +0800

    added restaurant controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,3 +1,46 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
 
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+#
+  # def edit
+  #   @restaurant = Restaurant.find(params[:id])
+  # end
+
+  # def update
+  #   @restaurant = Restaurant.find(params[:id])
+  #   @restaurant.update!(restaurant_params)
+  #   # raise
+  #   # No need for app/views/restaurants/update.html.erb
+  #   redirect_to restaurant_path(@restaurant)
+  # end
+
+  # def destroy
+  #   @restaurant = Restaurant.find(params[:id])
+  #   @restaurant.destroy
+  #   redirect_to index_path
+  # end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
 end
